c31b00901876d7d0306fff27d7ccf38a45f14243
Minorin <minorin@studio-mizutama.net>
2021-02-07 23:53:19 +0900

    update store.ts

--- src/stores.ts
+++ src/stores.ts
@@ -16,3 +16,5 @@ export const subColor = writable<string>('#FFFFFF');
 export const alpha = writable<number>(1);
 export const isMainOrSub = writable<boolean>(true);
 export const isPickerOrSwatch = writable<boolean>(true);
+
+export const defaultSwatch = writable<string[]>(['#000000', '#FFFFFF', '#FF0000','#00FF00', '#0000FF', null , '#FF80FF', '#80FFC0', '#FFFF00', '#8080FF', null, null, '#FF8000', '#00C0FF', '#80FFFF', '#FF8080']);
